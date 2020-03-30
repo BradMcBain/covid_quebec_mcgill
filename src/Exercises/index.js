a751b7c1b480b319eb904138e9c8fc5aa2e80599
BradMcBain <bradmcbaineng@gmail.com>
2020-03-30 15:13:33 -0400

    fixing left panels

--- src/Exercises/index.js
+++ src/Exercises/index.js
@@ -10,24 +10,55 @@ import Tableau7 from '../Tableau/Tableau7'
 import Tableau8 from '../Tableau/Tableau8'
 import { Paper } from "@material-ui/core"
 import PropTypes from 'prop-types';
+import Divider from '@material-ui/core/Divider'
+import { makeStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
 
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  chip: {
+    margin: theme.spacing(0.5),
+  },
+  section1: {
+    margin: theme.spacing(3, 2),
+  },
+  section2: {
+    margin: theme.spacing(2),
+  },
+  section3: {
+    margin: theme.spacing(3, 1, 1),
+  },
+}));
 
 const styles = {
-        Paper: { padding: 40, marginTop: 20, marginBottom:30 }
+        Paper: { padding: 30, marginTop: 20, marginBottom:30},
+        h3:{ color:"#334B63", align:"center"    }
 }
 
+
 function FormRow1(props) {
+  const classes = useStyles();
   if (props.lang === "en"){
     return (
       <React.Fragment>
         <Grid item xs={5}>
-             <Paper style={styles.Paper}>
-                Left pane
+             <Paper style={styles.Paper} elevation={5}>
+                 <h1 style={styles.h3}>French: Map of Quebec’s administrative region </h1>
+                <font size="3" color="#334B63" marginBottom="100">These two color-coded maps illustrate the number of confirmed cases in each of Quebec’s administrative regions. A darker shade of blue represents a higher number of cases. The left-most graphs depicts a map of the entire region of Quebec. The right-most graph zooms in to the southern regions of Quebec. You can see the exact number of cases in each region by clicking on the maps.
+                <br></br><br></br><br></br> </font>
+
+                <Divider variant="middle"/>
+                <h1 style={styles.h3}>Map of Quebec’s administrative region </h1>
+                <font size="3" color="#334B63" marginBottom="100"><br></br>These two color-coded maps illustrate the number of confirmed cases in each of Quebec’s administrative regions. A darker shade of blue represents a higher number of cases. The left-most graphs depicts a map of the entire region of Quebec. The right-most graph zooms in to the southern regions of Quebec. You can see the exact number of cases in each region by clicking on the maps. 
+                <br></br><br></br> </font>
             </Paper>
         </Grid>
         <Grid item xs={7}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={10}>
                 <Tableau1/>
             </Paper>
         </Grid>
@@ -37,12 +68,12 @@ function FormRow1(props) {
     return (
       <React.Fragment>
         <Grid item xs={5}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={5}>
                 Left pane
             </Paper>
         </Grid>
         <Grid item xs={7}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={10}>
                 <Tableau1/>
             </Paper>
         </Grid>
@@ -57,12 +88,12 @@ function FormRow2(props) {
     return (
       <React.Fragment>
         <Grid item xs={5}>
-            <Paper style={styles.Paper}>
+            <Paper style={styles.Paper} elevation={5}>
                 Left pane
             </Paper>
         </Grid>
         <Grid item xs={7}>
-            <Paper style={styles.Paper}>
+            <Paper style={styles.Paper} elevation={10}>
                 <Tableau2/>
             </Paper>
         </Grid>
@@ -72,12 +103,12 @@ function FormRow2(props) {
     return (
       <React.Fragment>
         <Grid item xs={5}>
-            <Paper style={styles.Paper}>
+            <Paper style={styles.Paper} elevation={5}>
                 Left pane
             </Paper>
         </Grid>
         <Grid item xs={7}>
-            <Paper style={styles.Paper}>
+            <Paper style={styles.Paper} elevation={10}>
                 <Tableau2/>
             </Paper>
         </Grid>
@@ -93,12 +124,12 @@ function FormRow3(props) {
   return (
     <React.Fragment>
       <Grid item xs={5}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={5}>
               Left pane
           </Paper>
       </Grid>
       <Grid item xs={7}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={10}>
               <Tableau3/>
           </Paper>
       </Grid>
@@ -108,12 +139,12 @@ function FormRow3(props) {
     return (
       <React.Fragment>
         <Grid item xs={5}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={5}>
                 Left pane
             </Paper>
         </Grid>
         <Grid item xs={7}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={10}>
                 <Tableau3/>
             </Paper>
         </Grid>
@@ -128,12 +159,12 @@ function FormRow4(props) {
   return (
     <React.Fragment>
       <Grid item xs={5}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={5}>
               Left pane
           </Paper>
       </Grid>
       <Grid item xs={7}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={10}>
               <Tableau4/>
           </Paper>
       </Grid>
@@ -143,12 +174,12 @@ function FormRow4(props) {
     return (
       <React.Fragment>
         <Grid item xs={5}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={5}>
                 Left pane
             </Paper>
         </Grid>
         <Grid item xs={7}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={10}>
                 <Tableau4/>
             </Paper>
         </Grid>
@@ -163,12 +194,12 @@ function FormRow5(props) {
   return (
     <React.Fragment>
       <Grid item xs={5}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={5}>
               Left pane
           </Paper>
       </Grid>
       <Grid item xs={7}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={10}>
               <Tableau5/>
           </Paper>
       </Grid>
@@ -178,12 +209,12 @@ function FormRow5(props) {
     return (
       <React.Fragment>
         <Grid item xs={5}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={5}>
                 Left pane
             </Paper>
         </Grid>
         <Grid item xs={7}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={10}>
                 <Tableau5/>
             </Paper>
         </Grid>
@@ -197,12 +228,12 @@ function FormRow6(props) {
   return (
     <React.Fragment>
       <Grid item xs={5}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={5}>
               Left pane
           </Paper>
       </Grid>
       <Grid item xs={7}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={10}>
               <Tableau6/>
           </Paper>
       </Grid>
@@ -212,12 +243,12 @@ function FormRow6(props) {
     return (
       <React.Fragment>
         <Grid item xs={5}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={5}>
                 Left pane
             </Paper>
         </Grid>
         <Grid item xs={7}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={10}>
                 <Tableau6/>
             </Paper>
         </Grid>
@@ -231,12 +262,12 @@ function FormRow7(props) {
   return (
     <React.Fragment>
       <Grid item xs={5}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={5}>
               Left pane
           </Paper>
       </Grid>
       <Grid item xs={7}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={10}>
               <Tableau7/>
           </Paper>
       </Grid>
@@ -246,12 +277,12 @@ function FormRow7(props) {
     return (
       <React.Fragment>
         <Grid item xs={5}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={5}>
                 Left pane
             </Paper>
         </Grid>
         <Grid item xs={7}>
-             <Paper style={styles.Paper}>
+             <Paper style={styles.Paper} elevation={10}>
                 <Tableau7/>
             </Paper>
         </Grid>
@@ -266,12 +297,12 @@ function FormRow8(props) {
   return (
     <React.Fragment>
       <Grid item xs={5}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={5}>
               Left pane
           </Paper>
       </Grid>
       <Grid item xs={7}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={10}>
               <Tableau8/>
           </Paper>
       </Grid>
@@ -281,12 +312,12 @@ function FormRow8(props) {
     return (
     <React.Fragment>
       <Grid item xs={5}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={5}>
               Left pane
           </Paper>
       </Grid>
       <Grid item xs={7}>
-           <Paper style={styles.Paper}>
+           <Paper style={styles.Paper} elevation={10}>
               <Tableau8/>
           </Paper>
       </Grid>
