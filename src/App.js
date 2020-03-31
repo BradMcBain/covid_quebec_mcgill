5d4af4b6e4d002e837f1e2202a66aa33c8f821e8
Brendan Marks <brendan99@live.ca>
2020-03-30 23:41:07 -0400

    error on mobile

--- src/App.js
+++ src/App.js
@@ -29,36 +29,46 @@ class App extends Component{
     }
   }
   render(){
-  return (
-    <div className="page-container">
-      <div className="content-wrap">
-        <Router basename={process.env.PUBLIC_URL}>
-          <Route component={App} path="/">
-            <Redirect from="*" to="/" />
-          </Route>
-        </Router>
-      <Drawer>
-      </Drawer>
-            <Footer
-              columns={[
-                {
-   
-                  title: 'This website and its contents herein, including all data, mapping, and analysis is provided to the public strictly for educational  purposes.  This data relies upon publicly available from Quebec\'s government (and foreign governments), which has been authorized for common usage. Reliance on the Website for medical guidance or use of the Website for commercial purposes is strictly prohibited.  Managers of this website are not responsible for source data or for any use of these data that goes beyond purely educational purposes.',
-                  url: '',
-                  description: '',
-                  openExternal: true,
-                },
-              ]}
-              style={{'paddingLeft':'12%','color':'white', 'fontSize':'10px'}}
-              bottom="Website Developed by Bradley McBain & Brendan Marks (McGill Engineering Students)"
-              theme="dark"
-              backgroundColor="#334B63"
-            />
-        </div>
-      </div>
-          
-        );
-        }
+    if (isMobile) {
+      return (
+        <div className="page-container">
+          <div className="content-wrap">
+              Mobile Site Unavailable, Please Visit Us at www.covidqc.com on Desktop.
+            </div>
+          </div>
+      );
+    } else {
+      return (
+        <div className="page-container">
+          <div className="content-wrap">
+            <Router basename={process.env.PUBLIC_URL}>
+              <Route component={App} path="/">
+                <Redirect from="*" to="/" />
+              </Route>
+            </Router>
+          <Drawer>
+          </Drawer>
+                <Footer
+                  columns={[
+                    {
+       
+                      title: 'This website and its contents herein, including all data, mapping, and analysis is provided to the public strictly for educational  purposes.  This data relies upon publicly available from Quebec\'s government (and foreign governments), which has been authorized for common usage. Reliance on the Website for medical guidance or use of the Website for commercial purposes is strictly prohibited.  Managers of this website are not responsible for source data or for any use of these data that goes beyond purely educational purposes.',
+                      url: '',
+                      description: '',
+                      openExternal: true,
+                    },
+                  ]}
+                  style={{'paddingLeft':'12%','color':'white', 'fontSize':'10px'}}
+                  bottom="Website Developed by Bradley McBain & Brendan Marks (McGill Engineering Students)"
+                  theme="dark"
+                  backgroundColor="#334B63"
+                />
+            </div>
+          </div>
+            );
+            }
+    }
+  
       }
 
 export default App;
