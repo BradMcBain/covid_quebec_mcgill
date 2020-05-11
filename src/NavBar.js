82a76c6a1962d399b19fead123a07d2c5931508a
Bogdan Tanasie <tanasie.bogdan21@gmail.com>
2020-05-11 12:33:04 -0400

    Improved rate change graphs

--- src/NavBar.js
+++ src/NavBar.js
@@ -120,7 +120,7 @@ class NavBar extends Component {
                               PROVINCE
                          </List>
                          <List>
-                             {['Map', 'Cases per day', 'Cases per type', 'Age distribution', 'Tests', 'Quebec vs. the world'].map((text, index) => (
+                             {['Map', 'Cases per day', 'Cases per type', 'Age distribution', 'Tests', 'Quebec vs. the world', 'Rate of change', 'Days to double'].map((text, index) => (
                              <ListItem button key={text} onClick={() => this.jumpTo(index + 5)}>
                                  <ListItemText primary={text} classes={{primary: classes.li}}/>
                              </ListItem>
@@ -203,7 +203,7 @@ class NavBar extends Component {
                              PROVINCE
                          </List>
                          <List>
-                             {['Carte', 'Cas par jour', 'Cas par type', 'Répartition par âge', 'Tests', 'Québec vs. monde'].map((text, index) => (
+                             {['Carte', 'Cas par jour', 'Cas par type', 'Répartition par âge', 'Tests', 'Québec vs. monde', 'Taux de variation', 'Jours pour doubler'].map((text, index) => (
                              <ListItem button key={text} onClick={() => this.jumpTo(index + 5)}>
                                  <ListItemText primary={text} classes={{primary: classes.li}}/>
                              </ListItem>
