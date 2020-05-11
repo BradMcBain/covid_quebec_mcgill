bcf2a498d09a9ee475ca66dc0236842b62816660
Loreina Chew <loreina.chew@gmail.com>
2020-05-11 16:45:28 -0400

    feat: create file for site config data

---
+++ data/SiteConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteTitle: "COVID QC", // Site title.
+  siteTitleShort: "COVID QC", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "COVID QC", // Alternative site title for SEO.
+  siteLogo: "/covidfavicon.ico", // Logo used for SEO and manifest.
+  siteUrl: "https://www.covidqc.com", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: "", // Website description used for RSS feeds/meta description tag.
+  siteFBAppID: "", // FB Application ID for using app insights
+  googleAnalyticsID: "UA-162445066", // GA tracking ID.
+};
